f4de84b6f6701bc3b6b5f05a7a4e39ca76bfc83a
Alix-cbh <alex.beecher@clipboardhealth.com>
2026-06-01 14:34:52 -0500

    TOPS - Update to Prod

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
-// export const apiUrl = 'https://wgxq2n4w71.execute-api.us-east-1.amazonaws.com/default/zendesk_get_wrap_up_notes';
-export const apiUrl = 'https://wgxq2n4w71.execute-api.us-east-1.amazonaws.com/default/zendesk_get_wrap_up_notes_staging';
+export const apiUrl = 'https://wgxq2n4w71.execute-api.us-east-1.amazonaws.com/default/zendesk_get_wrap_up_notes';
+// export const apiUrl = 'https://wgxq2n4w71.execute-api.us-east-1.amazonaws.com/default/zendesk_get_wrap_up_notes_staging';
 export const apiKey = 'wR1RqRe19qMAKa2CXjPa8f1C6HuzeoA9iYtLHHP8'; 
